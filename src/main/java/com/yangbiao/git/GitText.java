b170f72694feee56c7f6b02877c0c9da92519e24
yangbiao <3200410953@qq.com>
2023-02-07 22:43:00 +0800

    second commit

--- src/main/java/com/yangbiao/git/GitText.java
+++ src/main/java/com/yangbiao/git/GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello word");
+        System.out.println("Hello world");
     }
 }
