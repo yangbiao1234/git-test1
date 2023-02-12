8959d8f95f14f7e825e7f2e4b73acbef33938945
yangbiao <3200410953@qq.com>
2023-02-12 12:12:15 +0800

    push commit3

--- src/main/java/com/yangbiao/git/GitText.java
+++ src/main/java/com/yangbiao/git/GitText.java
@@ -13,6 +13,7 @@ public class GitText {
         System.out.println("Hello word21231321");
         System.out.println("Hello word21231321");
         System.out.println("Hello world");
+        System.out.println("Hello gitee");
 
     }
 }
