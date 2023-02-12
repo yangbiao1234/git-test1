926b5a6edc69e7fa9c81eabc137b6fe75cac2d4f
yangbiao1234 <107455613+yangbiao1234@users.noreply.github.com>
2023-02-12 11:35:24 +0800

    System.out.println("push Hello git");

--- src/main/java/com/yangbiao/git/GitText.java
+++ src/main/java/com/yangbiao/git/GitText.java
@@ -21,5 +21,6 @@ public class GitText {
         System.out.println("123");
         System.out.println("123");
         System.out.println("push Hello git");
+        System.out.println("push Hello git");
     }
 }
